2409cff8b7175c25474990b3d1fe8c5f5fc09f6f
Linda-Bley <lindaleyrosario@gmail.com>
2022-05-10 13:45:48 -0400

    cambios

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,86 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div class="container mt-12">
+    <aside class="text-h1 font-weight-bold text-center mt-10">
+      Mis Certificados
+    </aside>
+    <v-row class="mt-5">
+      <v-col
+        v-for="item, i in itemsCertificados"
+        :key="i"
+        class="col-3"
+      >
+        <viewer>
+          <v-card class="mx-auto">
+            <v-img
+              class="white--text align-end"
+              height="200px"
+              :src="item.img"
+            />
+            <v-card-actions>
+              <v-btn
+                color="orange"
+                text
+              >
+                Mintear
+              </v-btn>
+              <v-btn
+                :href="item.img"
+                download
+                color="orange"
+                text
+              >
+                Descargar
+              </v-btn>
+              <!-- <a
+                :href="item.img"
+                download
+                class="btn btn-success"
+              >
+                Descargar imagen
+              </a> -->
+            </v-card-actions>
+          </v-card>
+        </viewer>
+      </v-col>
+    </v-row>
+    <v-row class="mt-10">
+      <v-col class="col-6 offset-3">
+        <aside class="text-h2 font-weight-medium text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </aside>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="col-8 offset-2 text-center">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, deleniti obcaecati sed facilis, iste, ipsa voluptates suscipit ullam dolore quos sunt culpa officia vitae error quo at in eum commodi!
+        </p>
+        <v-btn
+          class="ma-2 mt-5"
+          rounded
+          outlined
+          large
+        >
+          Certificate
+        </v-btn>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+  export default {
+    name: 'DashboardDashboard',
+
+    data () {
+      return {
+        itemsCertificados: [
+          { img: require('../assets/img/certificado.png') },
+          { img: require('../assets/img/certificado.png') },
+          { img: require('../assets/img/certificado.png') },
+          { img: require('../assets/img/certificado.png') },
+        ],
+      }
+    },
+  }
 </script>

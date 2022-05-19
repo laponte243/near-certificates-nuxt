8a2779a8c3e236d913d79f96a09a8ab3d78603bf
Linda-Bley <lindaleyrosario@gmail.com>
2022-05-19 10:37:54 -0400

    cambios

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,6 +38,7 @@
             class="col-3 d-flex justify-end"
           >
             <div
+              v-show="!sesion"
               class="text-right"
             >
               <v-btn
@@ -45,9 +46,34 @@
                 rounded
                 outlined
                 large
+                @click="signIn()"
               >
                 Conectar Wallet
               </v-btn>
+            </div>
+            <div
+              v-show="sesion"
+              class="text-right"
+            >
+              <v-menu offset-y transition="slide-x-transition">
+                <template #activator="{ on, attrs }">
+                  <v-btn
+                    class="black--text ma-2"
+                    rounded
+                    outlined
+                    large
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    {{ accountId }}
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item @click="signOut()">
+                    <v-list-item-title>Cerrar Sesión</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
             </div>
           </v-col>
         </v-row>
@@ -65,6 +91,9 @@
 </template>
 
 <script>
+import * as nearAPI from 'near-api-js'
+import { CONFIG } from '~/services/api'
+const { connect, keyStores, WalletConnection, Contract } = nearAPI
 export default {
   name: 'DefaultLayout',
   data() {
@@ -72,7 +101,57 @@ export default {
       clipped: false,
       fixed: false,
       miniVariant: true,
+      sesion: false,
+      accountId: null,
     }
   },
+  mounted () {
+    this.isSigned()
+  },
+  methods: {
+    async signIn () {
+      // connect to NEAR
+      const near = await connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()))
+      // create wallet connection
+      const wallet = new WalletConnection(near)
+      wallet.requestSignIn(
+        'nft.nearcertificate.testnet'
+      )
+    },
+    async isSigned () {
+      // connect to NEAR
+      const near = await connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()))
+      // create wallet connection
+      const wallet = new WalletConnection(near)
+      if (wallet.isSignedIn()) {
+        const CONTRACT_NAME = 'book.bookshop2.testnet'
+        const contract = new Contract(wallet.account(), CONTRACT_NAME, {
+          viewMethods: ['get_profile'],
+          sender: wallet.account()
+        })
+        await contract.get_profile({
+          user_id: wallet.getAccountId()
+        }).then((res) => {
+          this.profilex = true
+        }).catch((err) => {
+          console.log(err)
+          this.profilex = false
+        })
+        this.sesion = true
+        // returns account Id as string
+        const walletAccountId = wallet.getAccountId()
+        this.accountId = walletAccountId
+      }
+    },
+    async signOut () {
+      // connect to NEAR
+      const near = await connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()))
+      // create wallet connection
+      const wallet = new WalletConnection(near)
+      wallet.signOut()
+      this.sesion = false
+      this.$router.go()
+    }
+  }
 }
 </script>

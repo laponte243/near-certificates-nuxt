7bbca3554622407815963b7ac6742b001e7fcb05
Linda-Bley <lindaleyrosario@gmail.com>
2022-05-25 16:00:04 -0400

    cambios

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
-    <aside class="text-h2 font-weight-bold text-center mt-8">
+    <aside class="text-lg-h2 text-h4 font-weight-bold text-center mt-8">
       Mis Certificados
     </aside>
     <v-row class="mt-5">
       <v-col
         v-for="item, i in dataCertificates"
         :key="i"
-        class="col-3"
+        class="col-lg-3 col-md-4 col-sm-6 col-12"
       >
         <viewer>
           <v-card class="mx-auto">
@@ -26,8 +26,6 @@
                 Mintear
               </v-btn>
               <v-btn
-                :href="item.img"
-                download
                 color="orange"
                 text
                 @click="download(item.img, item.certificacion)"
@@ -40,14 +38,14 @@
       </v-col>
     </v-row>
     <v-row class="mt-10">
-      <v-col class="col-6 offset-3">
-        <aside class="text-h4 font-weight-medium text-center">
+      <v-col class="col-12 col-md-6 offset-md-3">
+        <aside class="text-h5 text-lg-h4 font-weight-medium text-center">
           Aprende y construye la web 3.0
         </aside>
       </v-col>
     </v-row>
     <v-row>
-      <v-col class="col-8 offset-2 text-center">
+      <v-col class="col-12 col-md-8 offset-md-2 text-center">
         <p>
           Sabemos que deseas una guía experimentada que te ayude a conocer este nuevo mundo. Hemos creado experiencias de aprendizaje que transforman tu forma de percibir el blockchain, a tu propio ritmo y totalmente en línea.
         </p>
@@ -58,16 +56,45 @@
             outlined
             large
           >
-            Certificate
+            Certifícate
           </v-btn>
         </a>
       </v-col>
     </v-row>
+    <!-- <v-btn
+      dark
+      @click="snackbar = true"
+    >
+      Open Snackbar
+    </v-btn> -->
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="-1"
+      outlined
+      top
+      right
+    >
+      {{ textSnack }}
+      <v-progress-linear
+        indeterminate
+        color="black"
+      />
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+  import axios from 'axios'
   import * as nearAPI from 'near-api-js'
   import { CONFIG } from '~/services/api'
   const { connect, keyStores, WalletConnection, Contract } = nearAPI
@@ -76,14 +103,10 @@ import axios from 'axios'
     name: 'DashboardDashboard',
     data () {
       return {
-        itemsCertificados: [
-          { img: require('../assets/img/certificado.png') },
-          { img: require('../assets/img/certificado.png') },
-          { img: require('../assets/img/certificado.png') },
-          { img: require('../assets/img/certificado.png') },
-        ],
         dataCertificates: [],
         busqueda: false,
+        snackbar: false,
+        textSnack: 'Cargando',
       }
     },
     mounted() {
@@ -100,6 +123,7 @@ import axios from 'axios'
         this.$router.push('https://educacion.nearhispano.org/')
       },
       async viewCertificates (accountId) {
+        this.snackbar = true
         //alert(accountId)
         // connect to NEAR
         const near = await connect(
@@ -115,13 +139,16 @@ import axios from 'axios'
           account_id: accountId,
         }).then((response) => {
           this.dataCertificates = response
+          this.snackbar = false
           console.log(this.dataCertificates);
           localStorage.accountSearch = ''
         }).catch((err) => {
           console.log(err)
+          this.snackbar = false
         });
       },
       async mintCertificate (id) {
+        this.snackbar = true
         // connect to NEAR
         const near = await connect(
           CONFIG(new keyStores.BrowserLocalStorageKeyStore())
@@ -134,9 +161,14 @@ import axios from 'axios'
         });
         await contract.nft_mint({
           certificate_id: id,
-        }).then(response => {
+        },
+          '300000000000000',
+          '15200000000000000000000'
+        ).then(response => {
            console.log(response)
+           this.snackbar = false
         }).catch((err) => {
+          this.snackbar = false
           console.log(err)
         });
       },
@@ -150,8 +182,7 @@ import axios from 'axios'
           let link = document.createElement('a')
           link.style.display = 'none'
           link.href = url
-          link.setAttribute('download', 'certificado-' + certificacion + '.png')
-          
+          link.setAttribute('download', certificacion + '.png')
           document.body.appendChild(link)
           link.click()
         }).catch(error => {
